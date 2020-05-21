f3c7c117cc86e710eaa5d0e159916345fe6ed3a1
Joshua McArthur <jcmcarth06@gmail.com>
2020-05-21 19:56:11 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,10 +1,19 @@
 class Person
+<<<<<<< HEAD
  attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
  :weight, :handed, :complexion, :t_shirt_size,
+=======
+ attr_accessor :name, :birthday, :hair_color, :eye_color, :height, 
+ :weight, :handed, :complexion, :t_shirt_size, 
+>>>>>>> 056433a5551f959d8b4df2d8ced5116463a88390
  :wrist_size, :glove_size, :pant_length, :pant_width
 
  def initialize(attributes)
     attributes.each {|key, value| self.send(("#{key}="), value)}
   end
   
+<<<<<<< HEAD
 end
+=======
+end
+>>>>>>> 056433a5551f959d8b4df2d8ced5116463a88390
